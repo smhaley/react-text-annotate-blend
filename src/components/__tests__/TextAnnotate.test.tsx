1624440b4d3c53c647f4a372dcbc17b05d91b446
smhaley <shawnh87@gmail.com>
2024-04-06 02:33:46 -0400

    update tests

--- src/components/__tests__/TextAnnotate.test.tsx
+++ src/components/__tests__/TextAnnotate.test.tsx
@@ -20,39 +20,73 @@ const testVal = [
   },
 ];
 
-test("renders without getSpan", () => {
-  render(
-    <TextAnnotate
+export const testStyleAndClass = (
+  element: HTMLElement, 
+  container: HTMLElement, 
+  computedColor: string, 
+  className: string) => {
+  expect(window.getComputedStyle(element).color).toBe(computedColor)
+  expect(container.getElementsByClassName(className)).toHaveLength(1)
+}
+
+describe("TextAnnotate",()=>{
+  test("renders without getSpan", () => {
+    render(
+      <TextAnnotate
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
+      />
+    );
+  });
+
+  test("renders when value and getSpan return match", () => {
+    render(
+      <TextAnnotate
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "foo", extra: 1 }]}
+        onChange={() => {}}
+        getSpan={(span) => ({
+          ...span,
+          tag: "FOO",
+          text: "this ",
+          extra: 1,
+        })}
+      />
+    );
+  });
+
+  test("render duplicates overlapping tags -- with error message", () => {
+    render(
+      <TextAnnotate
+        content={testContent}
+        value={testVal}
+      />
+    );
+    const splits = ["this is a test", "tagA", "test", "tagB", ", wahoooo"];
+    splits.forEach((val) => expect(screen.getByText(val)).toBeTruthy());
+  });
+
+  test("passes mark style and classname", ()=>{
+    const {container} = render(<TextAnnotate
+        markClassName="blah"
+        markStyle={{color:"green"}}
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
+      />)
+
+    const mark = screen.getByText("this")
+    testStyleAndClass(mark, container, 'green', 'blah')
+  })
+
+  test("passes tag style and classname", ()=>{
+    const {container} = render(<TextAnnotate
+      tagClassName="blah"
+      tagStyle={{color:"green"}}
       content={testContent}
       value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
-    />
-  );
-});
+    />);
 
-test("renders when value and getSpan return match", () => {
-  render(
-    <TextAnnotate
-      content={testContent}
-      value={[{ start: 0, end: 5, tag: "PERSON", text: "foo", extra: 1 }]}
-      onChange={() => {}}
-      getSpan={(span) => ({
-        ...span,
-        tag: "FOO",
-        text: "this ",
-        extra: 1,
-      })}
-    />
-  );
-});
-
-test("render duplicates overlapping tags -- with error message", () => {
-  render(
-    <TextAnnotate
-      content={testContent}
-      value={testVal}
-      onChange={() => {}}
-    />
-  );
-  const splits = ["this is a test", "tagA", "test", "tagB", ", wahoooo"];
-  splits.forEach((val) => expect(screen.getByText(val)).toBeTruthy());
-});
+  const tag = screen.getByText("PERSON");
+  testStyleAndClass(tag, container, 'green', 'blah')
+  });
+})
--- src/components/__tests__/TextAnnotateBlend.test.tsx
+++ src/components/__tests__/TextAnnotateBlend.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TextAnnotateBlend from "../TextAnnotateBlend";
 import { render, screen } from "@testing-library/react";
+import { testStyleAndClass } from "./TextAnnotate.test";
 
 const testContent = "this is a test, wahoooo";
 const testVal = [
@@ -19,40 +20,64 @@ const testVal = [
     color: "#563C5C",
   },
 ];
+describe("TextAnnotateBlend", () => {
+  test("renders without getSpan", () => {
+    render(
+      <TextAnnotateBlend
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
+      />
+    );
+  });
+  
+  test("renders when value and getSpan return match", () => {
+    render(
+      <TextAnnotateBlend
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "foo", extra: 1 }]}
+        onChange={() => {}}
+        getSpan={(span) => ({
+          ...span,
+          tag: "FOO",
+          text: "this ",
+          extra: 1,
+        })}
+      />
+    );
+  });
+  
+  test("render does not duplicate blends", () => {
+    render(
+      <TextAnnotateBlend
+        content={testContent}
+        value={testVal}
+        onChange={() => {}}
+      />
+    );
+    const splits = ["this is a", "test", ", wahoooo"];
+    splits.forEach((val) => expect(screen.getByText(val)).toBeTruthy());
+  });
+  test("passes mark style and classname", ()=>{
+    const {container} = render(<TextAnnotateBlend
+        markClassName="blah"
+        markStyle={{color:"green"}}
+        content={testContent}
+        value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
+      />)
 
-test("renders without getSpan", () => {
-  render(
-    <TextAnnotateBlend
+    const mark = screen.getByText("this")
+    testStyleAndClass(mark, container, 'green', 'blah')
+  })
+
+  test("passes tag style and classname", ()=>{
+    const {container} = render(<TextAnnotateBlend
+      tagClassName="blah"
+      tagStyle={{color:"green"}}
       content={testContent}
       value={[{ start: 0, end: 5, tag: "PERSON", text: "this ", extra: 1 }]}
-    />
-  );
-});
+    />);
 
-test("renders when value and getSpan return match", () => {
-  render(
-    <TextAnnotateBlend
-      content={testContent}
-      value={[{ start: 0, end: 5, tag: "PERSON", text: "foo", extra: 1 }]}
-      onChange={() => {}}
-      getSpan={(span) => ({
-        ...span,
-        tag: "FOO",
-        text: "this ",
-        extra: 1,
-      })}
-    />
-  );
-});
-
-test("render does not duplicate blends", () => {
-  render(
-    <TextAnnotateBlend
-      content={testContent}
-      value={testVal}
-      onChange={() => {}}
-    />
-  );
-  const splits = ["this is a", "test", ", wahoooo"];
-  splits.forEach((val) => expect(screen.getByText(val)).toBeTruthy());
-});
+  const tag = screen.getByText("PERSON");
+  testStyleAndClass(tag, container, 'green', 'blah')
+  });
+})
